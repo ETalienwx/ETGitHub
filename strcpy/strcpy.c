c68d10870f323d68b872fa5f2e99567692a9e144
wangxuan <wangxuan0809@126.com>
2019-03-29 17:46:43 +0800

    strcpy

---
+++ strcpy/strcpy.c
@@ -0,0 +1,49 @@
+//Ä£ÄâÊµÏÖstrcpy
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <assert.h>
+//char * my_strcpy(char *dest, const char *src)
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//
+//	char* ret = dest;
+//	while (*src != *dest)
+//	{
+//		*dest = *src ;
+//		dest++;
+//		src++;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[] = { 0 };
+//	char arr2[] = "abcd";
+//	printf("%s",my_strcpy(arr1, arr2)); 
+//	system("pause");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <assert.h>
+//char * my_strcpy(char *dest, const char *src)
+//{
+//	assert(dest&&src);
+//	char *ret = dest;
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[] = { 0 };
+//	char arr2[] = "abcd";
+//	printf("%s\n", my_strcpy(arr1, arr2));
+//	system("pause");
+//	return 0;
+//}
